1ac92fc73e27c13a20e34d9b4b812a8f77531669
Ahyan <307210285+NightEyes0@users.noreply.github.com>
2026-09-17 23:42:25 +0100

    Implement regex to parse IP addresses and authentication status

--- analyser.py
+++ analyser.py
@@ -1,1 +1,28 @@
-print("Security Log Analyser initialized!")
+import re
+
+print("--- Log Analysis Started ---\n")
+
+# THE REGEX PATTERN:
+# \d{1,3} look for 1 to 3 numbers
+# \.  look for a literal dot
+# \s+ look for spaces
+# (SUCCESS|FAILED) look for either of these exact words
+log_pattern = r"(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+(SUCCESS|FAILED)"
+
+# Open log file in read
+with open("server.log", "r") as file:
+    
+    # Read the file 
+    for line in file:
+        
+        # Check if the line matches our Regex pattern
+        match = re.search(log_pattern, line)
+        
+        if match:
+            # group(1) pulls out the first thing in parentheses (the IP)
+            ip_address = match.group(1)
+            # group(2) pulls out the second thing in parentheses (the Status)
+            status = match.group(2)
+            
+            # The :<15 adds padding so the columns line up perfectly
+            print(f"Found event -> IP: {ip_address:<15} | Status: {status}")
