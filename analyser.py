68bf4c90565324afc5c81eb6077fb603870bb3d6
Ahyan <307210285+NightEyes0@users.noreply.github.com>
2026-09-18 16:55:38 +0100

    Add CSV export functionality to save security reports

--- analyser.py
+++ analyser.py
@@ -1,31 +1,25 @@
 import re
+import csv
 
 print("--- Log Analysis Started ---\n")
 
 log_pattern = r"(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+(SUCCESS|FAILED)"
-
-# A dictionary to keep track of failed logins per IP
 failed_logins = {}
 
 with open("server.log", "r") as file:
     for line in file:
         match = re.search(log_pattern, line)
-        
         if match:
             ip_address = match.group(1)
             status = match.group(2)
             
-            # If the login failed, add it to our tracking dictionary
             if status == "FAILED":
                 if ip_address in failed_logins:
                     failed_logins[ip_address] += 1
                 else:
                     failed_logins[ip_address] = 1
 
-# Security Logic 
 print("--- Security Report ---")
-
-# If an IP fails 3 or more times, flag it as a brute-force attack
 THRESHOLD = 3 
 
 for ip, count in failed_logins.items():
@@ -34,4 +28,21 @@ for ip, count in failed_logins.items():
     else:
         print(f"✅ IP {ip} had {count} failed attempt(s) (Under threshold)")
 
-print("\nAnalysis complete.")
+# --- NEW: Export to CSV ---
+print("\nExporting findings to CSV...")
+
+with open("security_report.csv", "w", newline="") as csv_file:
+    # Set up  CSV writer
+    writer = csv.writer(csv_file)
+    
+    # Write the header row
+    writer.writerow(["IP Address", "Failed Attempts", "Threat Level"])
+    
+    # Loop through our dictionary again and save each IP into the file
+    for ip, count in failed_logins.items():
+        if count >= THRESHOLD:
+            writer.writerow([ip, count, "Suspicious"])
+        else:
+            writer.writerow([ip, count, "Normal"])
+
+print("Successfully saved to 'security_report.csv'!")
